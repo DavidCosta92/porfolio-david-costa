590df69e8b42ac5ddb2aec23b8a217dc38db05a7
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2022-12-23 23:23:07 -0300

    agregando efecto de scroll snap

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,36 @@
+import tinyTypewriter from 'tiny-typewriter/src/';
+
+const typewriter = document.querySelector("#typewriter");
+tinyTypewriter(typewriter, {
+  items: ['Script', 'HTML']
+});
+
+tinyTypewriter(typewriter, {
+    // animation speed
+    typeSpeed: 100,
+    deleteSpeed: 50,
+    // delay between text
+    delayBetweenItems: 2000,
+    // infinite loop
+    loop: true,
+    // time to wait before starting the animation
+    startDelay: 0,
+    // start the animation at index n
+    startsAtIndex: 0,
+    // config animated cursor here
+    cursor: true,
+    cursorChar: "|",
+    cursorCharBlinkSpeed: 500,
+    cursorCharBlinkTransitionSpeed: 0.15,
+    // start the animation when the element is in the viewport
+    startOnView: true,
+    // offset for startOnView option
+    startOnViewOffset: 0
+    
+  });
+
+
+/*
 (function() {
     // https://dashboard.emailjs.com/admin/account
     emailjs.init('SY9HJBbS46YCiQfNz');
@@ -17,3 +50,5 @@ window.onload = function() {
             });
     });
 }
+
+*/
